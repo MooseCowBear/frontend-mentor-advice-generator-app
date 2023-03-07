086ba1293bf27886f363556252c957bb358f5421
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-03-07 17:39:20 -0500

    Add functions to get advice and display

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiUrl = "https://api.adviceslip.com/advice";
+
+getAdvice(); 
+
+//need to add event listener for click on dice to get new advice...
+
+async function getAdvice(){
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    console.log(data);
+
+    const id = data.slip.id;
+    const advice = data.slip.advice;
+
+    displayAdvice(id, advice);
+}
+
+function displayAdvice(id, advice) {
+    const adviceSpan = document.getElementById("advice");
+    const header = document.querySelector("h1");
+
+    header.innerText += `${id}`;
+    adviceSpan.innerText = advice;
+}
